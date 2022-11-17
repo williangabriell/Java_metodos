81475153e9756539400c2092eafa681b0d178ae6
williangabriell <willianlopes20@hotmail.com>
2022-11-17 11:25:46 -0300

    first commit

---
+++ Aula_1/src/Emprestimo.java
@@ -0,0 +1,48 @@
+/**
+ * Classe de exemplo para o exercício da Aula 1 de Métodos.
+ *
+ * Calcule o valor final de um empréstimo, a partir
+ * do valor solicitado. Taxas e parcelas influenciam.
+ * Defina arbitrariamente as faixas que influenciam
+ * nos valores.
+ */
+public class Emprestimo {
+
+    public static int getDuasParcelas() {
+        return 2;
+    }
+
+    public static int getTresParcelas() {
+
+        return 3;
+    }
+
+    public static double getTaxaDuasParcelas() {
+
+        return 0.3;
+    }
+
+    public static double getTaxaTresParcelas() {
+
+        return 0.45;
+    }
+
+    public static void calcular(double valor, int parcelas) {
+
+        if (parcelas == 2) {
+
+            double valorFinal = valor + (valor * getTaxaDuasParcelas());
+
+            System.out.println("Valor final do empréstimo para 2 parcelas: R$ " + valorFinal);
+        } else if (parcelas == 3) {
+
+            double valorFinal = valor + (valor * getTaxaTresParcelas());
+
+            System.out.println("Valor final do empréstimo para 3 parcelas: R$ " + valorFinal);
+        } else {
+            System.out.println("Quantidade de parcelas não aceita.");
+        }
+
+    }
+
+}
---
+++ Aula_3/src/Quadrilatero.java
@@ -0,0 +1,33 @@
+/**
+ * Classe de exemplo para o exercício da Aula 3 de Métodos.
+ *
+ * Recrie a aplicação que calcula a área dos 3 quadriláteros notáveis. Agora faça os métodos retornarem valores.
+ */
+public class Quadrilatero {
+
+    public static double area(double lado) {
+
+        return lado * lado;
+    }
+
+    public static double area(double lado1, double lado2) {
+
+        return lado1 * lado2;
+    }
+
+    public static double area(double baseMaior, double baseMenor, double altura) {
+
+        return ((baseMaior+baseMenor)*altura) / 2;
+    }
+
+    public static void xpto() {
+
+        System.out.println("Antes");
+        return;
+    }
+
+    public static double abc() {
+        return 1.6;
+    }
+
+}
